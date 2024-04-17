cd5697488bd218620093b3f14ab9e067d09fffd7
jay-so <merry9504@naver.com>
2024-04-17 16:33:55 +0900

    test: AddressDepth 도메인 테스트

---
+++ src/test/java/com/eodya/backend/fixture/domain/AddressDepthFixture.java
@@ -0,0 +1,24 @@
+package com.eodya.backend.fixture.domain;
+
+import com.eodya.backend.address.domain.AddressDepth1;
+import com.eodya.backend.address.domain.AddressDepth2;
+
+public class AddressDepthFixture {
+
+    public static AddressDepth1 addressDepth1Build() {
+        String addressDepth1Name = "서울시";
+
+        return AddressDepth1.builder()
+                .name(addressDepth1Name)
+                .build();
+    }
+
+    public static AddressDepth2 addressDepth2Build(AddressDepth1 addressDepth1) {
+        String addressDepth2Name = "강남구";
+
+        return AddressDepth2.builder()
+                .name(addressDepth2Name)
+                .addressDepth1(addressDepth1)
+                .build();
+    }
+}
